a84d401b803a0a757ab11842de717d8be2c61d7f
Avni <avnirastogi363@gmail.com>
2025-02-23 10:38:24 +0530

    login hf code

--- llm.py
+++ llm.py
@@ -1,1 +1,18 @@
+from huggingface_hub import login
 
+# Replace "YOUR_HUGGINGFACE_TOKEN" with your actual token
+login(token="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")
+
+import torch
+from transformers import pipeline
+
+model_id = "meta-llama/Llama-3.2-1B"
+
+pipe = pipeline(
+    "text-generation",
+    model=model_id,
+    torch_dtype=torch.bfloat16,
+    device_map="auto"
+)
+
+pipe("The key to life is")
